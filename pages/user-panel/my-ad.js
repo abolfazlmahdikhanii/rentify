b9a727a6e919c2d9fc9102fe533ee0605255143b
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2025-05-21 18:24:12 +0330

    show ad and remove ad

--- pages/user-panel/my-ad.js
+++ pages/user-panel/my-ad.js
@@ -2,34 +2,59 @@ import Home from "@/components/module/Home/Home";
 import Content from "@/components/module/UserPanel/Content/Content";
 import EmptyList from "@/components/module/UserPanel/EmptyList/EmptyList";
 import DashboardLayout from "@/components/templates/UserPanel/DashboardLayout";
+import { toastOption } from "@/helper/helper";
+import { getCookie } from "cookies-next";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import useSWR from "swr";
 
-
 const fetcher = () =>
-  fetch("http://localhost:5000/api/properties/user-ad", {
+  fetch("http://localhost:5000/api/properties/user-ads", {
     method: "GET",
-    headers: { Authorization: `Bearer ${getCookie("token")}` },
-  }).then((res) => res.json());
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getCookie("token")}`,
+    },
+  }).then((res) => {
+    if (res.ok) return res.json();
+  });
 const MyAdvertisement = () => {
-  const { data, isLoading } = useSWR("ad", fetcher);
-  
+  const { data, isLoading,mutate } = useSWR("user-ad", fetcher);
+
+  const removeAdHandler = (id) => {
+    fetch(`http://localhost:5000/api/properties/${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${getCookie("token")}`,
+      },
+    }).then((res) => {
+      if (res.ok) {
+        toast.success("ملک با موفقیت حذف شد", toastOption);
+        mutate()
+      } else {
+        toast.error("حذف ملک خطا مواجه شد", toastOption);
+      }
+    });
+  };
   return (
     <DashboardLayout title="آگهی‌های ذخیره شده">
       <Content>
-        {/* <div className="fav-grid">
-        {data?.splice(0, 6).map((home) => (
-          <Home key={home.id} {...home} isBorder={true} isMyAd={true} />
-        ))}
-        </div> */}
-        <EmptyList
-          src={"/images/empty-add-ad.png"}
-          title="شما هنوز آگهی‌ای ثبت نکردید!"
-          subtitle="روزانه هزاران مشتری در رنتی‌فای در جستجوی ملک مورد نظرشان هستند"
-          btnText="ثبت آگهی ‌رایگان"
-          type="add"
-        />
+        {data?.data && data?.data.length > 0 ? (
+          <div className="fav-grid">
+            {data?.data.map((home) => (
+              <Home key={home.id} {...home} isBorder={true} isMyAd={true} remove={()=>removeAdHandler(home.id)} />
+            ))}
+          </div>
+        ) : (
+          <EmptyList
+            src={"/images/empty-add-ad.png"}
+            title="شما هنوز آگهی‌ای ثبت نکردید!"
+            subtitle="روزانه هزاران مشتری در رنتی‌فای در جستجوی ملک مورد نظرشان هستند"
+            btnText="ثبت آگهی ‌رایگان"
+            type="add"
+          />
+        )}
       </Content>
     </DashboardLayout>
   );
