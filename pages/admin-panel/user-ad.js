b257f091f51118a8114f8400286f8025cc401b8f
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2025-06-04 18:48:32 +0330

    add PropertyDialog and DeleteModal components; implement property approval, rejection, and deletion functionality

--- pages/admin-panel/user-ad.js
+++ pages/admin-panel/user-ad.js
@@ -1,11 +1,16 @@
+import PropertyDialog from "@/components/AdminPanel/PropertyDialog/PropertyDialog";
 import { PropertyTable } from "@/components/AdminPanel/PropertyTable/PropertyTable";
+import DeleteModal from "@/components/module/DeleteModal/DeleteModal";
 import Home from "@/components/module/Home/Home";
 import Content from "@/components/module/UserPanel/Content/Content";
 import EmptyList from "@/components/module/UserPanel/EmptyList/EmptyList";
 import DashboardLayout from "@/components/templates/UserPanel/DashboardLayout";
+import { getDate, toastOption } from "@/helper/helper";
 import { getCookie } from "cookies-next";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import { set } from "react-hook-form";
+import { toast } from "react-toastify";
 import useSWR from "swr";
 
 const fetcher = () =>
@@ -14,9 +19,13 @@ const fetcher = () =>
     headers: { Authorization: `Bearer ${getCookie("token")}` },
   }).then((res) => res.json());
 const MyAdvertisement = () => {
-  const { data, isLoading } = useSWR("admin-ad", fetcher);
+  const { data, isLoading, mutate } = useSWR("admin-ad", fetcher);
   const [page, setPage] = useState(1);
   const [newAd, setNewAd] = useState([]);
+  const [adDetail, setAdDetail] = useState(null);
+  const [isOpenDiaog, setIsOpenDialog] = useState(false);
+  const [propertyDetail, setPropertyDetail] = useState(null);
+  const [isOpenDeleteModal, setIsOpenDeleteModal] = useState(false);
   useEffect(() => {
     if (!data?.data) return;
     const ads = [...data?.data];
@@ -29,6 +38,66 @@ const MyAdvertisement = () => {
     setPage(page);
   };
 
+  const approveHandler = (id) => {
+    fetch(`http://localhost:5000/api/properties/${id}/approve`, {
+      method: "PATCH",
+      headers: { Authorization: `Bearer ${getCookie("token")}` },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setIsOpenDialog(false);
+        setPropertyDetail(null);
+        toast.success("ملک با موفقیت ثبت شد", toastOption);
+        mutate("admin-ad");
+      })
+      .catch((err) => {
+        setIsOpenDialog(false);
+        toast.error("خطا در ثبت ملک", toastOption);
+      });
+  };
+  const rejectHandler = (id, reason) => {
+    fetch(`http://localhost:5000/api/properties/${id}/reject`, {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${getCookie("token")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ reason }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setIsOpenDialog(false);
+        setPropertyDetail(null);
+        mutate("admin-ad");
+        toast.success("ملک با موفقیت رد شد", toastOption);
+      })
+      .catch((err) => {
+        setIsOpenDialog(false);
+        toast.error("خطا در رد ملک", toastOption);
+      });
+  };
+  const deleteHandler = (data) => {
+    setPropertyDetail(data);
+    setIsOpenDeleteModal(true);
+  };
+  const deleteProperty = (id) => {
+    fetch(`http://localhost:5000/api/properties/${id}`, {
+      method: "DELETE",
+      headers: { Authorization: `Bearer ${getCookie("token")}` },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setIsOpenDeleteModal(false);
+        setIsOpenDialog(false);
+        setPropertyDetail(null);
+        mutate("admin-ad");
+        toast.success("ملک با موفقیت حذف شد", toastOption);
+      })
+      .catch((err) => {
+        setIsOpenDeleteModal(false);
+        toast.error("خطا در حذف ملک", toastOption);
+      });
+  };
   return (
     <DashboardLayout title="آگهی‌های ذخیره شده" role="admin">
       <Content type="tbl">
@@ -39,11 +108,11 @@ const MyAdvertisement = () => {
           totalPages={Math.ceil(data?.count / 8)}
           currPage={page}
           startIndex={(page - 1) * 8}
-          endIndex={page*8}
-          total={data.count}
+          endIndex={page * 8}
+          total={data?.count || data?.data?.length}
         >
           <tbody className="tbody">
-            {data?.data.length > 0
+            {data?.data?.length > 0
               ? newAd.map((item) => (
                   <tr key={item.id}>
                     <td>
@@ -60,7 +129,7 @@ const MyAdvertisement = () => {
 
                     <td className="tbl-txt tbl-txt-2">{item.title}</td>
                     <td className="tbl-txt">{item.author}</td>
-                    <td className="tbl-txt">1402/07/15</td>
+                    <td className="tbl-txt">{getDate(item.created_at)}</td>
                     <td className="tbl-txt">
                       <span className={`status status__${[item.status]}-2`}>
                         {item.status === "pending" && "در انتظار ثبت"}
@@ -70,7 +139,13 @@ const MyAdvertisement = () => {
                     </td>
 
                     <td>
-                      <button className="btn btn-outline-4">
+                      <button
+                        className="btn btn-outline-4"
+                        onClick={() => {
+                          setAdDetail(item);
+                          setIsOpenDialog(true);
+                        }}
+                      >
                         نمایش جزییات
                       </button>
                     </td>
@@ -79,10 +154,32 @@ const MyAdvertisement = () => {
               : null}
           </tbody>
         </PropertyTable>
+
+        {isOpenDiaog && (
+          <PropertyDialog
+            isOpen={isOpenDiaog}
+            onClose={() => setIsOpenDialog(false)}
+            property={adDetail}
+            approveHandler={approveHandler}
+            rejectHandler={rejectHandler}
+            deleteHandler={deleteHandler}
+          />
+        )}
+
         {/* <EmptyList
           src={"/images/empty-add-ad.png"}
           title="شما هنوز آگهی‌ای ثبت نکردید!"
         /> */}
+        {isOpenDeleteModal && (
+          <DeleteModal
+            isOpen={isOpenDeleteModal}
+            onClose={() => setIsOpenDeleteModal(false)}
+            title="حذف ملک"
+            question="آیا از حذف این ملک اطمینان دارید؟ این عملیات قابل بازگشت نیست."
+            property={propertyDetail}
+            onConfirm={() => deleteProperty(propertyDetail.id)}
+          />
+        )}
       </Content>
     </DashboardLayout>
   );
