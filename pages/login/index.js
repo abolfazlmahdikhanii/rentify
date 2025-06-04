08391096ac07773d27b02adabb695b7a3dc05579
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2025-06-04 21:18:02 +0330

    refactor LoginPage component; enhance OTP handling, add email validation, and implement profile setup functionality

--- pages/login/index.js
+++ pages/login/index.js
@@ -7,8 +7,9 @@ import Input from "@/components/module/Form/Input";
 import OtpInput from "react-otp-input";
 import { set, useForm } from "react-hook-form";
 import { useRouter } from "next/router";
-import { setCookie } from "cookies-next";
-import { timeFormat } from "@/helper/helper";
+import { getCookie, setCookie } from "cookies-next";
+import { timeFormat, toastOption } from "@/helper/helper";
+import { toast } from "react-toastify";
 
 export default function LoginPage() {
   const route = useRouter();
@@ -17,6 +18,7 @@ export default function LoginPage() {
   const [otpValue, setOtpValue] = useState("");
   const [countdown, setCountdown] = useState(180);
   const [isResendDisabled, setIsResendDisabled] = useState(false);
+  const [isVerify, setIsVerify] = useState(false);
   const {
     register,
     watch,
@@ -117,7 +119,18 @@ export default function LoginPage() {
     }
   };
   const sendOtp = () => {
-    const isValid = validationFormHandler(activeTab);
+    const fields = [
+      {
+        name: "email",
+        required: true,
+        validate: (value) => {
+          const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+          return emailRegex.test(value) || "ایمیل معتبر نیست";
+        },
+      },
+    ];
+    const isValid = checkValidate(fields);
+
     if (isValid) {
       setOtp(true);
       const email = getValues("email");
@@ -135,14 +148,15 @@ export default function LoginPage() {
         .then((data) => {
           if (data.success) {
             console.log(data);
+            toast.success("کد تایید به ایمیل شما ارسال شد", toastOption);
           }
           // Set otp to true after sending OTP
         })
         .catch((error) => {
-          console.error("Error sending OTP:", error);
+          toast.error("خطا در ارسال کد تایید", toastOption);
         });
     } else {
-      console.log("Validation failed");
+      toast.error("لطفا ایمیل معتبر وارد کنید", toastOption);
     }
   };
 
@@ -155,9 +169,9 @@ export default function LoginPage() {
       body: JSON.stringify({
         email: getValues("email"),
         otp: otpValue,
-        name: getValues("name"),
-        lastName: getValues("last_name"),
-        agencyName: getValues("agency_name"),
+        // name: getValues("name"),
+        // lastName: getValues("last_name"),
+        // agencyName: getValues("agency_name"),
       }),
     })
       .then((response) => {
@@ -168,14 +182,19 @@ export default function LoginPage() {
         return response.json();
       })
       .then((data) => {
-        console.log("OTP verified:", data);
-        if (data.token) {
-          setCookie("token", data.token, { maxAge: 60 * 60 * 24 * 7 });
-          route.replace("/");
+        if (data.success) {
+          console.log("OTP verified:", data);
+          if (!data.needsProfileSetup) {
+            setCookie("token", data.token, { maxAge: 60 * 60 * 24 * 7 });
+            route.replace("/");
+          } else {
+            setCookie("token", data.token, { maxAge: 60 * 60 * 24 });
+            setIsVerify(true);
+          }
         }
       })
       .catch((error) => {
-        console.error("Error verifying OTP:", error);
+        toast.error("کد تایید اشتباه است", toastOption);
       });
   };
   const resendOtp = () => {
@@ -194,13 +213,45 @@ export default function LoginPage() {
         if (data.success) {
           setIsResendDisabled(true);
           setCountdown(180);
+          toast.success("کد تایید مجدد ارسال شد", toastOption);
         }
       })
       .catch((error) => {
-        console.error("Error resending OTP:", error);
+        toast.error("خطا در ارسال کد تایید", toastOption);
       });
   };
 
+  const setupProfile = () => {
+    const isValid = validationFormHandler(activeTab);
+    if (isValid) {
+      const formData = {
+        name: getValues("name"),
+        lastName: getValues("last_name"),
+        agencyName: getValues("agency_name"),
+      };
+      fetch("http://localhost:5000/api/auth/setup-profile", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${getCookie("token")}`,
+        },
+        body: JSON.stringify(formData),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.success) {
+            setCookie("token", data.token, { maxAge: 60 * 60 * 24 * 7 });
+            toast.success("پروفایل با موفقیت ایجاد شد", toastOption);
+            setTimeout(() => {
+              route.replace("/");
+            }, 600);
+          }
+        })
+        .catch((error) => {
+          toast.error("خطا در ایجاد پروفایل", toastOption);
+        });
+    }
+  };
   return (
     <div className={styles.containerRow}>
       <div className={styles.rightSection}>
@@ -212,158 +263,47 @@ export default function LoginPage() {
 
         <div className={styles.formContainer}>
           <h2 className={styles.formTitle}>
-            {!otp ? "ورود | ثبت نام" : "کد تایید"}
+            {!isVerify && !otp && "ورود | ثبت نام"}
+            {!isVerify && otp && "کد تایید"} {isVerify && "تکمیل پروفایل"}
           </h2>
-          {!otp ? (
-            <>
-              <div className={styles.tabs}>
-                <button
-                  className={`${styles.tab} ${
-                    activeTab === "house" ? styles.activeTab : ""
-                  }`}
-                  onClick={() => {
-                    setActiveTab("house");
-                    reset({
-                      name: "",
-                      last_name: "",
-                      agency_name: "",
-                      phone: "",
-                    });
-                  }}
-                >
-                  مالک | مستاجر
-                </button>
-                <button
-                  className={`${styles.tab} ${
-                    activeTab === "agency" ? styles.activeTab : ""
-                  }`}
-                  onClick={() => {
-                    setActiveTab("agency");
-                    reset({
-                      name: "",
-                      last_name: "",
-                      agency_name: "",
-                      phone: "",
-                    });
-                  }}
-                >
-                  آژانس املاک
-                </button>
-              </div>
-
+          {!isVerify && !otp && (
+            <div className={styles.formBody}>
               <div className={styles.formGroup}>
-                {activeTab === "house" ? (
-                  <form onSubmit={handleSubmit(sendOtp)}>
-                    <div className={styles.formRow}>
-                      <div>
-                        <Input
-                          label="نام"
-                          placeholder="نام را وارد کنید"
-                          val={watch("name")}
-                          error={errors.name}
-                          onChange={(val) => {
-                            setValue("name", val, {
-                              shouldValidate: true,
-                            });
-                          }}
-                        />
-                      </div>
-                      <Input
-                        label=" نام خانوادگی"
-                        placeholder=" نام خانوادگی را وارد کنید"
-                        val={watch("last_name")}
-                        error={errors.last_name}
-                        onChange={(val) =>
-                          setValue("last_name", val, {
-                            shouldValidate: true,
-                          })
-                        }
-                      />
-                    </div>
+                <form onSubmit={handleSubmit(sendOtp)}>
+                  <div className={styles.emailRow}>
+                    {" "}
                     <Input
+                      type="email"
                       label="ایمیل"
                       placeholder="ایمیل را وارد کنید"
                       error={errors.email}
-                      val={watch("email")}
+                      val={getValues("email")}
+                      size="lg"
                       onChange={(val) =>
                         setValue("email", val, {
                           shouldValidate: true,
                         })
                       }
                     />
-                  </form>
-                ) : (
-                  <form onSubmit={handleSubmit(sendOtp)}>
-                    <div className={styles.formRow}>
-                      <div>
-                        <Input
-                          label="نام"
-                          error={errors.name}
-                          placeholder="نام را وارد کنید"
-                          val={watch("name")}
-                          onChange={(val) =>
-                            setValue("name", val, {
-                              shouldValidate: true,
-                            })
-                          }
-                        />
-                      </div>
-                      <Input
-                        label="نام خانوادگی"
-                        error={errors.last_name}
-                        placeholder="نام خانوادگی را وارد کنید"
-                        val={watch("last_name")}
-                        onChange={(val) =>
-                          setValue("last_name", val, {
-                            shouldValidate: true,
-                          })
-                        }
-                      />
-                    </div>
-                    <div className={styles.emailRow}>
-                      <Input
-                        label="نام دفتر"
-                        placeholder="نام دفتر را وارد کنید"
-                        error={errors.agency_name}
-                        val={watch("agency_name")}
-                        onChange={(val) =>
-                          setValue("agency_name", val, {
-                            shouldValidate: true,
-                          })
-                        }
-                      />
-                    </div>
-                    <div className={styles.emailRow}>
-                      {" "}
-                      <Input
-                        label="ایمیل"
-                        placeholder="ایمیل را وارد کنید"
-                        error={errors.email}
-                        val={watch("email")}
-                        onChange={(val) =>
-                          setValue("email", val, {
-                            shouldValidate: true,
-                          })
-                        }
-                      />
-                    </div>
-                  </form>
-                )}
+                  </div>
+                </form>
               </div>
 
               <div className={styles.rememberMe}>
                 <input type="checkbox" id="remember" />
                 <label htmlFor="remember">مرا به خاطر بسپار</label>
               </div>
-            </>
-          ) : (
+            </div>
+          )}
+          {!isVerify && otp && (
             <>
               <div className={styles.otpInfo}>
                 <p className={styles.otpText}>
-                  کد ارسال شده به شماره موبایل {getValues("phone")} را وارد کنید
+                  کد ارسال شده به شماره ایمیل{" "}
+                  <span dir="ltr">{getValues("email")}</span> را وارد کنید
                 </p>
                 <p className={styles.otpEdit} onClick={() => setOtp(false)}>
-                  ویرایش موبایل
+                  ویرایش ایمیل
                 </p>
               </div>
               <OtpInput
@@ -415,7 +355,7 @@ export default function LoginPage() {
                   </defs>
                 </svg>
                 <p className={styles.otpTimeText}>
-                  {countdown >0 ? (
+                  {countdown > 0 ? (
                     <>
                       <span className={styles.otpTimer}>
                         {timeFormat(countdown)}
@@ -423,19 +363,138 @@ export default function LoginPage() {
                       تا دریافت مجدد کد
                     </>
                   ) : (
-                    <button className="btn pageLink" onClick={resendOtp}>دریافت کد مجدد</button>
+                    <button className="btn pageLink" onClick={resendOtp}>
+                      دریافت کد مجدد
+                    </button>
                   )}
                 </p>
               </div>
             </>
           )}
-          <button
-            className={styles.loginButton}
-            disabled={otp ? otpValue.length < 4 : false} // Call the function here
-            onClick={!otp ? sendOtp : verifyOtp}
-          >
-            {otp ? "تایید کد" : "ورود و دریافت کد"}
-          </button>
+          {isVerify && (
+            <>
+              <div className={styles.tabs}>
+                <button
+                  className={`${styles.tab} ${
+                    activeTab === "house" ? styles.activeTab : ""
+                  }`}
+                  onClick={() => {
+                    setActiveTab("house");
+                    reset({
+                      name: "",
+                      last_name: "",
+                      agency_name: "",
+                    });
+                  }}
+                >
+                  مالک | مستاجر
+                </button>
+                <button
+                  className={`${styles.tab} ${
+                    activeTab === "agency" ? styles.activeTab : ""
+                  }`}
+                  onClick={() => {
+                    setActiveTab("agency");
+                    reset({
+                      name: "",
+                      last_name: "",
+                      agency_name: "",
+                    });
+                  }}
+                >
+                  آژانس املاک
+                </button>
+              </div>
+
+              <div className={styles.formGroup}>
+                {activeTab === "house" ? (
+                  <form onSubmit={handleSubmit(setupProfile)}>
+                    <div className={styles.formRow}>
+                      <div>
+                        <Input
+                          label="نام"
+                          placeholder="نام را وارد کنید"
+                          val={watch("name")}
+                          error={errors.name}
+                          onChange={(val) => {
+                            setValue("name", val, {
+                              shouldValidate: true,
+                            });
+                          }}
+                        />
+                      </div>
+                      <Input
+                        label=" نام خانوادگی"
+                        placeholder=" نام خانوادگی را وارد کنید"
+                        val={watch("last_name")}
+                        error={errors.last_name}
+                        onChange={(val) =>
+                          setValue("last_name", val, {
+                            shouldValidate: true,
+                          })
+                        }
+                      />
+                    </div>
+                  </form>
+                ) : (
+                  <form onSubmit={handleSubmit(setupProfile)}>
+                    <div className={styles.formRow}>
+                      <div>
+                        <Input
+                          label="نام"
+                          error={errors.name}
+                          placeholder="نام را وارد کنید"
+                          val={watch("name")}
+                          onChange={(val) =>
+                            setValue("name", val, {
+                              shouldValidate: true,
+                            })
+                          }
+                        />
+                      </div>
+                      <Input
+                        label="نام خانوادگی"
+                        error={errors.last_name}
+                        placeholder="نام خانوادگی را وارد کنید"
+                        val={watch("last_name")}
+                        onChange={(val) =>
+                          setValue("last_name", val, {
+                            shouldValidate: true,
+                          })
+                        }
+                      />
+                    </div>
+                    <div className={styles.emailRow}>
+                      <Input
+                        label="نام دفتر"
+                        placeholder="نام دفتر را وارد کنید"
+                        error={errors.agency_name}
+                        val={watch("agency_name")}
+                        onChange={(val) =>
+                          setValue("agency_name", val, {
+                            shouldValidate: true,
+                          })
+                        }
+                      />
+                    </div>
+                  </form>
+                )}
+              </div>
+            </>
+          )}
+          {!isVerify ? (
+            <button
+              className={styles.loginButton}
+              disabled={!isVerify && otp ? otpValue.length < 4 : false} // Call the function here
+              onClick={!isVerify && !otp ? sendOtp : verifyOtp}
+            >
+              {!isVerify && otp ? "تایید کد" : "ورود و دریافت کد"}
+            </button>
+          ) : (
+            <button className={styles.loginButton} onClick={setupProfile}>
+              تکمیل پروفایل
+            </button>
+          )}
         </div>
       </div>
       <div className={styles.leftSection}>
