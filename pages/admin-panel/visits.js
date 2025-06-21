a011df1ec4aac088c1b664274b94a46c2028b2ea
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2025-06-21 23:07:57 +0330

    feat: implement Visits component to display admin visit records with conditional rendering for empty state

---
+++ pages/admin-panel/visits.js
@@ -0,0 +1,37 @@
+import { PropertyTable } from "@/components/AdminPanel/PropertyTable/PropertyTable";
+import VisitCard from "@/components/AdminPanel/VisitCard/VisitCard";
+import Content from "@/components/module/UserPanel/Content/Content";
+import EmptyList from "@/components/module/UserPanel/EmptyList/EmptyList";
+import DashboardLayout from "@/components/templates/UserPanel/DashboardLayout";
+import { getCookie } from "cookies-next";
+import React, { useState } from "react";
+import useSWR from "swr";
+
+const fetcher = () =>
+  fetch("http://localhost:5000/api/visits/admin", {
+    method: "GET",
+    headers: { Authorization: `Bearer ${getCookie("token")}` },
+  }).then((res) => res.json());
+const Visits = () => {
+  const { data, isLoading, mutate } = useSWR("visits", fetcher);
+  const [page, setPage] = useState(1);
+  const [newVisits, setNewVisits] = useState([]);
+console.log(data);
+  return (
+    <DashboardLayout title="بازدیدها" role="admin">
+      <Content type="tbl">
+        {data?.length ? (
+          <VisitCard />
+        ) : (
+          <EmptyList
+            src={"/images/empty-add-ad.png"}
+            title="شما هنوز آگهی‌ای ثبت نکردید!"
+            noBtn={true}
+          />
+        )}
+      </Content>
+    </DashboardLayout>
+  );
+};
+
+export default Visits;
