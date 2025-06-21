c9ad3119d1973df62f29af24a54cf3052c70b730
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2025-06-21 23:06:01 +0330

    refactor DeleteModal component; enhance UI with info icon for non-delete actions and improve styling for dialog headers

--- components/module/DeleteModal/DeleteModal.jsx
+++ components/module/DeleteModal/DeleteModal.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./DeleteModal.module.css";
-import { TriangleAlert } from "lucide-react";
+import { Info, TriangleAlert } from "lucide-react";
 const DeleteModal = ({
   isOpen,
   onClose,
@@ -8,6 +8,7 @@ const DeleteModal = ({
   title,
   question,
   property,
+  type = "delete",
 }) => {
   return (
     <>
@@ -23,11 +24,26 @@ const DeleteModal = ({
           aria-describedby="dialog-description"
           aria-modal="true"
         >
-          <div className={styles.dialogHeader}>
-            <div className={styles.warningIcon} aria-hidden="true">
-              <TriangleAlert />
-            </div>
-            <h2 id="dialog-title" className={styles.dialogTitle}>
+          <div
+            className={
+              type === "delete" ? styles.dialogHeader : styles.dialogHeader2
+            }
+          >
+            {type === "delete" ? (
+              <div className={styles.warningIcon} aria-hidden="true">
+                <TriangleAlert />
+              </div>
+            ) : (
+              <div className={styles.infoIcon} aria-hidden="true">
+                <Info />
+              </div>
+            )}
+            <h2
+              id="dialog-title"
+              className={
+                type === "delete" ? styles.dialogTitle : styles.dialogTitle2
+              }
+            >
               {title}
             </h2>
           </div>
@@ -37,35 +53,65 @@ const DeleteModal = ({
               {question}
             </p>
 
-            <div className={styles.propertyInfo}>
-              <div className={styles.propertyTitle}>{property.title}</div>
-              <div className={styles.propertyAddress}>{property.location}</div>
-            </div>
-
-            <div className={styles.buttonGroup}>
-              <button
-                className={`${styles.btn} ${styles.btnDelete}`}
-                onClick={onConfirm}
-                // disabled={loading}
-                // aria-describedby={loading ? "loading-description" : undefined}
-              >
-                {/* {loading && (
+            {property && (
+              <div className={styles.propertyInfo}>
+                <div className={styles.propertyTitle}>{property.title}</div>
+                <div className={styles.propertyAddress}>
+                  {property.location}
+                </div>
+              </div>
+            )}
+            {type === "delete" ? (
+              <div className={styles.buttonGroup}>
+                <button
+                  className={`${styles.btn} ${styles.btnDelete}`}
+                  onClick={onConfirm}
+                  // disabled={loading}
+                  // aria-describedby={loading ? "loading-description" : undefined}
+                >
+                  {/* {loading && (
                   <span
                     className={styles.loadingSpinner}
                     aria-hidden="true"
                   ></span>
                 )} */}
-                حذف
-                {/* {loading ? loadingText : confirmText} */}
-              </button>
-              <button
-                className={`${styles.btn} ${styles.btnCancel}`}
-                onClick={onClose}
-                // disabled={loading}
-              >
-                لغو
-              </button>
-            </div>
+                  حذف
+                  {/* {loading ? loadingText : confirmText} */}
+                </button>
+                <button
+                  className={`${styles.btn} ${styles.btnCancel}`}
+                  onClick={onClose}
+                  // disabled={loading}
+                >
+                  لغو
+                </button>
+              </div>
+            ) : (
+              <div className={styles.buttonGroup}>
+                <button
+                  className={`${styles.btn} ${styles.btnModify}`}
+                  onClick={onConfirm}
+                  // disabled={loading}
+                  // aria-describedby={loading ? "loading-description" : undefined}
+                >
+                  {/* {loading && (
+                  <span
+                    className={styles.loadingSpinner}
+                    aria-hidden="true"
+                  ></span>
+                )} */}
+                  تغییر
+                  {/* {loading ? loadingText : confirmText} */}
+                </button>
+                <button
+                  className={`${styles.btn} ${styles.btnCancel}`}
+                  onClick={onClose}
+                  // disabled={loading}
+                >
+                  لغو
+                </button>
+              </div>
+            )}
 
             {/* {loading && (
               <div id="loading-description" className="sr-only">
