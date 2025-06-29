fd5111548b9a08acbfdf907ed8f9ce68d9b43912
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2025-06-29 20:40:21 +0330

    enhance Title component to conditionally apply margin based on isComment prop

--- components/module/Title/Title.js
+++ components/module/Title/Title.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Title.module.css";
-const Title = ({ title }) => {
-  return <h4 className={styles.title}>{title}</h4>;
+const Title = ({ title,isComment=false }) => {
+  return <h4 className={`${styles.title} ${!isComment?styles.titleMb4:""}`}>{title}</h4>;
 };
 
 export default Title;
