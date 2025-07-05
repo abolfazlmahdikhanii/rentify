1090702e1769e711d2dc34b28d1cb4c18d499b39
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2025-07-05 15:06:53 +0330

    refactor InfoCards component to accept dynamic data; enhance styling for icons and value display

--- components/AdminPanel/InfoCard/InfoCard.js
+++ components/AdminPanel/InfoCard/InfoCard.js
@@ -1,40 +1,10 @@
 import styles from "./infocard.module.css";
 
-export function InfoCards() {
-  const cards = [
-    {
-      title: "درآمد کل",
-      value: "۴۵,۲۳۱,۰۰۰",
-      unit: "تومان",
-      change: "+۲۰.۱٪",
-      icon: "money",
-    },
-    {
-      title: "املاک اجاره‌ای",
-      value: "۲,۳۵۰",
-      unit: "ملک",
-      change: "+۱۸.۱٪",
-      icon: "home",
-    },
-    {
-      title: "قراردادهای جدید",
-      value: "۱,۲۳۴",
-      unit: "قرارداد",
-      change: "+۱۹٪",
-      icon: "document",
-    },
-    // {
-    //   title: "بازدیدهای امروز",
-    //   value: "۵۷۳",
-    //   unit: "بازدید",
-    //   change: "+۲۰۱",
-    //   icon: "chart",
-    // },
-  ]
+export function InfoCards({data}) {
 
   return (
     <div className={styles.infoCards}>
-      {cards.map((card, index) => (
+      {data?.map((card, index) => (
         <div key={index} className={styles.infoCard}>
           <div className={styles.infoCardHeader}>
             <h3 className={styles.infoCardTitle}>{card.title}</h3>
@@ -44,7 +14,7 @@ export function InfoCards() {
             <div className={styles.infoCardValue}>
               {card.value} <span className={styles.infoCardUnit}>{card.unit}</span>
             </div>
-            <div className={styles.infoCardChange}>{card.change} نسبت به ماه قبل</div>
+            <div className={`${styles.infoCardChange} ${card.changeType==="increase"?styles.increase:styles.decrease}`}>{card.change!==null?card.change:card.changeType==="increase"?1:-1} نسبت به ماه قبل</div>
           </div>
         </div>
       ))}
