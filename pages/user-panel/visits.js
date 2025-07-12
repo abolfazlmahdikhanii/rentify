8c7984359f4134ed6e5b653a05b46fb2798b9bd5
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2025-07-12 20:21:45 +0330

    create page for show visits user

---
+++ pages/user-panel/visits.js
@@ -0,0 +1,141 @@
+import DeleteModal from "@/components/module/DeleteModal/DeleteModal";
+import Home from "@/components/module/Home/Home";
+import MapSelect from "@/components/module/Map/MapSelect";
+import Pagination from "@/components/module/Pagination/Pagination";
+import Content from "@/components/module/UserPanel/Content/Content";
+import EmptyList from "@/components/module/UserPanel/EmptyList/EmptyList";
+import UserVisitBox from "@/components/module/UserPanel/UserVisitBox/UserVisitBox";
+import ModalMap from "@/components/templates/RegisterStep/ModalMap";
+import DashboardLayout from "@/components/templates/UserPanel/DashboardLayout";
+import { toastOption } from "@/helper/helper";
+import { getCookie } from "cookies-next";
+import Image from "next/image";
+import React, { use, useEffect, useState } from "react";
+import { set } from "react-hook-form";
+import { toast } from "react-toastify";
+import useSWR from "swr";
+
+const fetcher = () =>
+  fetch("http://localhost:5000/api/visits/user", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getCookie("token")}`,
+    },
+  }).then((res) => {
+    if (res.ok) return res.json();
+  });
+const MyAdvertisement = () => {
+  const { data, isLoading, mutate } = useSWR("user-visit", fetcher);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [newData, setNewData] = useState([]);
+  const [position, setPosition] = useState(null);
+  const [isShowModal, setIsShowModal] = useState(false);
+const [selectedVisit, setSelectedVisit] = useState(null);
+  const [isOpenCancelModal, setIsOpenCancelModal] = useState(false);
+  const onPageChange = (page) => {
+    setCurrentPage(page);
+  };
+  const cancelVisitHandler = (id) => {
+    fetch(`http://localhost:5000/api/visits/${id}/cancel`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getCookie("token")}`,
+      },
+    
+    })
+      .then((res) => {
+        if (res.status !== 200) {
+          toast.error("خطا در لغو بازدید", toastOption);
+        } else if (res.ok) {
+          toast.success("بازدید با موفقیت لغو شد", toastOption);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setIsOpenCancelModal(false);
+
+        mutate();
+      })
+      .catch((err) => {
+        setIsOpenCancelModal(false);
+
+        toast.error("خطا در لغو بازدید", toastOption);
+      });
+  };
+  const paginationData = data
+    ? data.slice((currentPage - 1) * 9, currentPage * 9)
+    : [];
+  console.log(data);
+  return (
+    <DashboardLayout title="آگهی‌های ذخیره شده">
+      <Content>
+        {data && data?.length > 0 ? (
+          <>
+            <div className="fav-grid">
+              {paginationData.map((home) => (
+                <Home
+                  key={home.id}
+                  {...home}
+                  isBorder={true}
+                  isMyVisit={true}
+                  onShowMap={(pos) => {
+                    setIsShowModal(true);
+                    setPosition(pos);
+                  }}
+                  onCancel={() => {
+                    setSelectedVisit(home);
+                    setIsOpenCancelModal(true);
+                  }}
+                  // remove={() => removeAdHandler(home.id)}
+                />
+              ))}
+            </div>
+            <Pagination
+              totalPages={Math.ceil(data.length / 10)}
+              currentPage={currentPage}
+              onPageChange={onPageChange}
+            />
+          </>
+        ) : (
+          <EmptyList
+            src={"/images/empty-add-ad.png"}
+            title="شما هنوز آگهی‌ای ثبت نکردید!"
+            subtitle="روزانه هزاران مشتری در رنتی‌فای در جستجوی ملک مورد نظرشان هستند"
+            btnText="ثبت آگهی ‌رایگان"
+            type="add"
+          />
+        )}
+      </Content>
+      {isShowModal && (
+        <ModalMap
+          onClose={() => setIsShowModal(false)}
+          isConfirm={false}
+          title="مشاهده موقعیت ملک"
+        >
+          <MapSelect
+            isEnable={false}
+            isVisit={true}
+            position={position}
+            setPosition={setPosition}
+          />
+        </ModalMap>
+      )}
+      {isOpenCancelModal && (
+        <DeleteModal
+          isOpen={isOpenCancelModal}
+          onClose={() => setIsOpenCancelModal(false)}
+          property={selectedVisit}
+          title="لغو بازدید"
+          question="آیا از لغو بازدید این ملک اطمینان دارید؟ این عملیات قابل بازگشت نیست."
+          onConfirm={() => cancelVisitHandler(selectedVisit.id)}
+          btnText="لغو بازدید"
+        
+        />
+      )}
+    </DashboardLayout>
+  );
+};
+
+export default MyAdvertisement;
